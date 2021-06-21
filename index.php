791424dfdf9a98eb0c00bc64ace3162410a823fc
CriminalShrimp <aleisterlima@yahoo.com.br>
2021-06-21 19:38:07 -0300

    Rpfda

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$today = date("F j, Y, g:i a"); 
+echo "today";
+?>
+<html>
+<head>
+</head>
+<header>
+</header>
+<body>
+    <h1>Avaliação SOCPS II</h1>
+    <h2>Aleister Edward de Lima</h2>
+    <p><?php echo "today"?></p>
+</body>
+</html>
